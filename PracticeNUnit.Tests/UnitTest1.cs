9364955ac376348d9688489eb8fd31f3a6f8a196
avvakumov <avvakumov@tslog.ru>
2023-02-03 13:14:45 +0300

    Add project files.

---
+++ PracticeNUnit.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PracticeNUnit.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
